1dc4bbff9c42823b02ae9b37f5dd17fd622bb4b8
User-Takhirov <tohirovfayruz@gmail.com>
2024-08-01 17:46:55 +0500

    js entrance

---
+++ main.js
@@ -0,0 +1,118 @@
+// let a=700;
+// let b=100;
+// let d=(a/b);
+// console.log(d);
+
+
+
+// let a=50;
+// let b=10;
+// let d=(a/b);
+// console.log(d);
+
+
+// let a=7;
+// let b=2;
+// let d=(a%b);
+// console.log(d);
+
+
+// let a=52;
+// let b=a%10;
+// let c=(a-b)/10;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// let a=62;
+// let b=10;
+// let c=a%b;
+// let d=(a-c)/b;
+// console.log(c+d);
+
+
+
+
+// let a=12;
+// let b=a%10;
+// let c=(a-b)/10;
+// console.log(b*10+c);
+
+
+// yuzlar xonasini chiqarish
+// let a=399;
+// let b=a%100;
+// let c=(a-b)/100;
+// console.log(c);
+
+
+// o'nlar va birlar xonasini chiqarish
+// let a=398;
+// let b=a%100;
+// let c=b%10;
+// let d=(b-c)/10;
+// console.log(d);
+// console.log(c);
+
+
+// sonning raqamlar yigindisi
+// let a=398;
+// let b=a%100;
+// let c=b%10;
+// let d=(b-c)/10;
+// let e=(a-b)/100;
+// console.log(e+d+c);
+
+// 341ni teskarisini chiqarish
+// let a=341;
+// let b=a%100;
+// let c=(a-b)/100;
+// let d=(b- (a%10))/10;
+// let e=b%10;
+// let f=(e*100+d*10+c)
+// console.log(f);
+
+
+// Homework
+
+//14.
+// let a=968;
+// let b = a%100;
+// let c=(a-b)/100;
+// let d= (b-(a%10))/10;
+// let e=a%10;
+// let f=(e*100+d*10+c);
+// console.log(f);
+
+// 15.
+// let a=367;
+// let b = a%100;
+// let c=(a-b)/100;
+// let d= (b-(a%10))/10;
+// let e=a%10;
+// let f=(d*100+c*10+e);
+// console.log(f);
+
+
+// 16.
+// let a=743;
+// let b = a%100;
+// let c=(a-b)/100;
+// let d= (b-(a%10))/10;
+// let e=a%10;
+// let f=(c*100+e*10+d);
+// console.log(f);
+
+// 17.
+// let a=1432;
+// let b=a%1000;
+// let c=(a-b)/1000;
+// let d=(b-(a%100))/100;
+// console.log(d);
+
+// 18.
+// let a=1326;
+// let b=a%1000;
+// let c=(a-b)/1000
+// console.log(c);
